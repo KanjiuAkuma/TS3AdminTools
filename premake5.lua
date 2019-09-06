501cb0fa8e877c6585da9919428bb48b2334b1f0
Kanjiu Akuma <18683538+KanjiuAkuma@users.noreply.github.com>
2019-09-06 15:41:20 +0200

    Renamed to TS3...

--- premake5.lua
+++ premake5.lua
@@ -4,7 +4,7 @@ bin_int_dir = build_dir .. "%{prj.name}\\bin-int\\%{cfg.buildcfg}-%{cfg.system}-
 
 ts_plugin_dir = "C:/Users/Kanjiu Akuma/AppData/Roaming/TS3Client/plugins"
 
-workspace "Ts3AdminTools"
+workspace "TS3AdminTools"
 	architecture "x64"
 
 	configurations
@@ -13,37 +13,36 @@ workspace "Ts3AdminTools"
 		"Release",
 	}
 
-project "Ts3AdminTools"
-	location "Ts3AdminTools"
+project "TS3AdminTools"
+	location "TS3AdminTools"
 	language "C++"
 	cppdialect "C++17"
 	systemversion "latest"
+	kind "SharedLib"
 
 	targetdir("%{wks.location}/" .. bin_dir)
 	objdir("%{wks.location}/" .. bin_int_dir)
+
+	files {
+		"%{prj.name}/src/**",
+	}
 
 	includedirs {
 		"%{prj.name}/src",
 		"%{prj.name}/src/vendor",
 	}
 
-	files {
-		"%{prj.name}/src/**",
-	}
-
 	postbuildcommands {
-		"copy /Y \"%{wks.location}" .. bin_dir .. "\\Ts3AdminTools.dll\" \"" .. ts_plugin_dir .. "/\""
+		"copy /Y \"%{wks.location}" .. bin_dir .. "\\%{prj.name}.dll\" \"" .. ts_plugin_dir .. "/\""
 	}
 
 	filter "configurations:Debug"
 		symbols "Full"
-		kind "SharedLib"
 		defines {
 			"AT_DEBUG"
 		}
 
 	filter "configurations:Release"
-		kind "SharedLib"
 		optimize "On"
 		defines {
 			"AT_RELEASE"
